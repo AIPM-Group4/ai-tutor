4004a94312c6fa559b72557308e34e66bb3e7626
Leilabenj <leila.benjelloun@epfl.ch>
2024-10-06 21:32:51 +0200

    Saving audio file works for the model tag currentl y in use.

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,15 +3,18 @@ import soundfile as sf
 from espnet2.bin.tts_inference import Text2Speech
 import os
 from espnet_model_zoo.downloader import ModelDownloader
+import shutil
+from huggingface_hub import hf_hub_download
+from pathlib import Path
+import nltk
 
 
-d = ModelDownloader()  # <module_dir> is used as cachedir by default
-model = d.download_and_unpack("https://zenodo.org/record/3986231/files/tts_train_fastspeech_raw_phn_tacotron_g2p_en_no_space_train.loss.best.zip?download=1")
+nltk.download('averaged_perceptron_tagger_eng')
 
 
 def text_to_speech(text):
     # Load the pre-trained TTS model
-    text2speech = Text2Speech.from_pretrained("model")
+    text2speech = Text2Speech.from_pretrained(model_tag="kan-bayashi/ljspeech_tacotron2")
 
     # Perform text-to-speech
     speech = text2speech(text)["wav"]
@@ -24,6 +27,12 @@ def text_to_speech(text):
 
     # Save the output to a WAV file in the "Audio" folder
     sf.write(output_path , speech.numpy(), text2speech.fs, "PCM_16")
+
+
+
+
+
+
 
 # Test the function
 text = "Hello, this is a test of the text-to-speech function."
