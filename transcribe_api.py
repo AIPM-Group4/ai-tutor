fee34d017354938ab89d882dc2fa8140cc42adbb
MasaNakura <masanakura43104@hotmail.com>
2024-10-15 18:52:04 +0200

    transcribe bytes

--- transcribe_api.py
+++ transcribe_api.py
@@ -17,4 +17,12 @@ def process_speech_to_text(audio_file, lang="en"):
     )
     return transcript
 
+def process_speech_bytes_to_text(file_type, file_bytes, content_type, lang="en"):
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=("temp." + file_type, file_bytes, content_type),
+        response_format="text",
+        language=lang,
+    )
+    return transcript
 # print(process_speech_to_text("New Recording 37.m4a"))
