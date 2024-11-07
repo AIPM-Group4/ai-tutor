1188729a050dc728fe1243d46928d8ea33839d1e
Omar El Malki <homerdon0@gmail.com>
2024-11-07 16:00:06 +0100

    Clean up repository

--- dialogue.py
+++ dialogue.py
@@ -1,61 +1,70 @@
-from langchain_ollama import OllamaLLM
+import os
+from openai import OpenAI
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+
+load_dotenv()
 
 HISTORY_LENGTH = 100
-TEMPERATURE = 0.5
-MODELS = ["llama3.2:1b", "llama3.2"]
-MODEL = 1
+MODEL = "gpt-4o"  # You can change this to other OpenAI models as needed
 
 class Model():
-    def __init__(self, language="français") -> None:
+    def __init__(self, language="english") -> None:
         self.language = language
         self.history = []
 
-        # Get model
-        model = OllamaLLM(model=MODELS[MODEL], temperature=TEMPERATURE)
+        # Initialize OpenAI client
+        self.client = OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )  # Make sure to set your API key in the environment variable
 
         # Define behaviour
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "Tu es un système qui engage des conversations avec l'utilisateur afin qu'il apprenne {language}. Tu parles en {language}. Tu es gentil et énergique. Tu es ouvert à parler de nombreux sujets. Vérifie si l'utilisateur fait des erreurs grammaticales lorsqu'il parle et essaie de les corriger subtilement en continuant la conversation. Par exemple, tu peux répéter ce qu'il a dit dans le cadre de ta phrase de manière naturelle. Ne corrige pas trop. Ignore les erreurs grammaticales moins importantes qu'il fait. Si l'erreur grammaticale est très importante, tu peux la signaler et la corriger directement, mais toujours continuer la conversation. Encourage l'utilisateur à parler. Sois bref dans tes réponses. Il doit parler plus que toi pour apprendre. Commence par te présenter."),
-                MessagesPlaceholder(variable_name="history"),
-                ("human", "{query}")
-            ]
-        )
-
-        self.chain = prompt | model
+        # self.system_prompt = "Tu es un système qui engage des conversations avec l'utilisateur afin qu'il apprenne {language}. Tu parles en {language}. Tu es gentil et énergique. Tu es ouvert à parler de nombreux sujets. Vérifie si l'utilisateur fait des erreurs grammaticales lorsqu'il parle et essaie de les corriger subtilement en continuant la conversation. Par exemple, tu peux répéter ce qu'il a dit dans le cadre de ta phrase de manière naturelle. Ne corrige pas trop. Ignore les erreurs grammaticales moins importantes qu'il fait. Si l'erreur grammaticale est très importante, tu peux la signaler et la corriger directement, mais toujours continuer la conversation. Encourage l'utilisateur à parler. Sois bref dans tes réponses. Il doit parler plus que toi pour apprendre. Commence par te présenter."
+        self.system_prompt = "You should output you response in this format: <response> | <list of errors and their corrections>. You are a French native speaker speaking with a user who is learning French. You are a helpful assistant. You should output you response in this format: <response> | <list of errors and their corrections>."
 
     def process(self, query: str) -> str:
-        output = self.chain.invoke(
-            {
-                "language": self.language,
-                "history": self.history,
-                "query": query
-            })
+        messages = [{"role": "system", "content": self.system_prompt.format(language=self.language)}]
+        messages.extend([{"role": "user" if i % 2 == 0 else "assistant", "content": msg.content} for i, msg in enumerate(self.history)])
+        messages.append({"role": "user", "content": query})
+
+        response = self.client.chat.completions.create(
+            model=MODEL,
+            messages=messages,
+        )
+        output = response.choices[0].message.content
+
         self.history.append(HumanMessage(content=query))
         self.history.append(AIMessage(content=output))
         if len(self.history) > HISTORY_LENGTH:
-            self.history.pop[0]
-        return output
+            self.history.pop(0)
+        
+        if "|" in output:
+            response, errors = output.split("|")
+            return response, errors
+        else:
+            return output, ""
     
     # Only for testing
     def stream(self, query: str) -> None:
-        output = ""
-        for chunk in self.chain.stream(
-            {
-                "language": self.language,
-                "history": self.history,
-                "query": query
-        }):
-            #if isinstance(chunk, AIMessage):
-            print(chunk, end="", flush=True)
+        messages = [{"role": "system", "content": self.system_prompt.format(language=self.language)}]
+        messages.extend([{"role": "user" if i % 2 == 0 else "assistant", "content": msg.content} for i, msg in enumerate(self.history)])
+        messages.append({"role": "user", "content": query})
 
-            output += chunk
+        output = ""
+        for chunk in self.client.chat.completions.create(
+            model=MODEL,
+            messages=messages,
+            stream=True
+        ):
+            if chunk.choices[0].delta.content is not None:
+                print(chunk.choices[0].delta.content, end="", flush=True)
+                output += chunk.choices[0].delta.content
+
         self.history.append(HumanMessage(content=query))
         self.history.append(AIMessage(content=output))
         if len(self.history) > HISTORY_LENGTH:
-            self.history.pop[0]
+            self.history.pop(0)
         return output
     
 # Only for testing
--- prototype.py
+++ prototype.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import os
-import sys
-sys.path.append(os.path.join(os.getcwd(), "ai-tutor"))
-import gtts
-from gtts import gTTS
-from audio_processing import process_speech_to_text
-from text_to_speech import output_audio
-from dialogue import Model
+import io
 
+from transcribe import process_speech_to_text, process_speech_bytes_to_text
+from tts import output_audio_gtts
+from dialogue import Model
+from pydub import AudioSegment
+from audio_recorder_streamlit import audio_recorder
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TEST_MODE = False
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+        
 # Initialize session state for chat history and flags
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
@@ -16,11 +26,17 @@ if "audio_processed" not in st.session_state:
 if "text_sent" not in st.session_state:
     st.session_state.text_sent = False
 if "conversation_active" not in st.session_state:
-    st.session_state.conversation_active = False
+    st.session_state.conversation_active = True
+if "audio_bytes" not in st.session_state:
+    st.session_state.audio_bytes = None
 
 # Function to display chat messages in bubbles
 def display_chat():
     for message in st.session_state.chat_history:
+        display_message(message)
+
+def display_message(message):
+    with st.chat_message(message["user"]):
         if message["user"] == "user":
             st.markdown(
                 f"<div style='text-align: right; background-color: #d1e7ff; color: black; padding: 10px; border-radius: 10px; margin: 10px 0; width: fit-content; float: right;'>{message['text']}</div>",
@@ -31,75 +47,56 @@ def display_chat():
                 f"<div style='text-align: left; background-color: #f0f0f0; color: black; padding: 10px; border-radius: 10px; margin: 10px 0; width: fit-content; float: left;'>{message['text']}</div>",
                 unsafe_allow_html=True,
             )
-    # Clear floating elements after chat bubbles
-    st.markdown("<div style='clear: both;'></div>", unsafe_allow_html=True)
-
-# 音声ファイルを生成する関数
-def output_audio(text, filename="output.mp3"):
-    tts = gTTS(text)
-    tts.save(filename)
-    print(f"Audio file saved as: {filename}")
+            if 'audio_bytes' in message and message['audio_bytes']: # If statement for prod/test mode
+                st.audio(message['audio_bytes'], format="audio/wav", autoplay=True)
 
 # Title
 st.title("AI Tutor")
+# Chatbot model response
+model = Model()
+model.process("You are a system that engages in conversations with the user to help them learn french. You start by asking in English what kind of roleplay you are going to do. Then you start the roleplay in french. If the user asks you to speak in English, you do so. Otherwise, try to use simpler words if the user struggles with the language. You are a helpful assistant. You should output you response in this format: <response> | <list of errors and their corrections>.")
 
-# Display chat bubbles
-display_chat()
-
-# Step 1: Upload an audio file
-uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3", "m4a"])
-
-if uploaded_file is not None:
-    # Ensure the audio processing happens only once
-    if not st.session_state.audio_processed:
-        st.session_state.chat_history.append({"user": "user", "text": "Voice file uploaded."})
-        st.audio(uploaded_file, format="audio/wav")
-
-        # Process the uploaded audio file to text
-        try:
-            st.markdown("Processing audio file...")
-            text = process_speech_to_text(uploaded_file)
-            st.session_state.chat_history.append({"user": "user", "text": text})
+if st.session_state.conversation_active:
+    display_chat()
+    if audio_bytes := audio_recorder(icon_size="4x", pause_threshold=7):
+        # Ensure the audio processing happens only once
+        if not st.session_state.audio_processed:
+            # Process the audio to text
+            st.markdown("Processing audio...")
+            if not TEST_MODE:
+                text = process_speech_bytes_to_text('wav', audio_bytes, 'audio/wav', lang='fr')
+                message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
+            else:
+                text = 'text'
+                message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
+            st.session_state.chat_history.append(message)
             st.session_state.audio_processed = True  # Set flag to True after processing
             st.session_state.audio_text = text  # Store the processed text in session state
-            display_chat()
-        except Exception as e:
-            st.error(f"Error processing the audio file: {e}")
-            st.session_state.chat_history.append({"user": "ai", "text": "Failed to process the audio file."})
+            st.rerun()
 
-# Step 2: Send the text to chatbot model
-if "audio_text" in st.session_state and st.session_state.audio_processed and not st.session_state.text_sent:
-    if st.button("Send"):
-        st.session_state.chat_history.append({"user": "user", "text": "Processing..."})
-        display_chat()
+        # Step 2: Send the text to chatbot model
+        if "audio_text" in st.session_state and st.session_state.audio_processed and not st.session_state.text_sent:
+            if not TEST_MODE:
+                response, errors = model.process(st.session_state.audio_text)
+            else:
+                response = 'Bonjour, comment allez vous? Voulez-vous apprendre le français?'
+                errors = ''
+            st.session_state.text_sent = True  # Set flag to True after response is generated
+            # Step 3: Convert the chatbot's response to speech and play
+            st.markdown("Playing response...")
+            audio = output_audio_gtts(response, 'fr')
+            #audio = output_audio(response, stream=True)
+            message = {"user": "assistant", "text": response, "audio_bytes": audio}
+            st.session_state.chat_history.append(message)
+            st.session_state.chat_history.append({"user": "assistant", "text": "Errors identified: " + errors, "audio_bytes": None})
+            st.rerun()
 
-        # Chatbot model response
-        model = Model()
-        response = model.process(st.session_state.audio_text)
-        st.session_state.chat_history.append({"user": "ai", "text": response})
-        # show the response text in the chat
-        st.session_state.text_sent = True  # Set flag to True after response is generated
-        display_chat()
+if st.session_state.conversation_active and st.session_state.text_sent:
+    # Reset flags for new audio input
+    st.session_state.text_sent = False
+    st.session_state.audio_processed = False
 
-        # Step 3: Convert the chatbot's response to speech and play
-        st.markdown("Playing response...")
-        output_audio(response)
-
-        # Check if output.mp3 was successfully created
-        output_path = os.path.join(os.getcwd(), "output.mp3")
-        if os.path.exists(output_path):
-            st.audio(output_path, format="audio/mp3")
-            st.session_state.chat_history.append({"user": "ai", "text": "Playing response..."})
-        else:
-            st.error("Error: Audio file not found. Could not play the response.")
-
-# conversation continues
-if st.session_state.text_sent:
-    if st.button("Continue conversation"):
-        # Reset flags for new audio input
-        st.session_state.text_sent = False
-        st.session_state.audio_processed = False
-        st.session_state.conversation_active = True  # Keep the conversation active
-        st.write("Upload a new audio file to continue the conversation.")
-    else:
-        st.write("Conversation ended. Upload a new audio file to start a new conversation.")
+#if st.button("Finish conversation"):
+    #st.session_state.conversation_active = False  # Keep the conversation active
+    #st.session_state.text_sent = True
+    #st.session_state.audio_processed = True
