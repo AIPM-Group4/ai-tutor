1f73150defa553df0e8bc561ab923e492d13e848
MasaNakura <masanakura43104@hotmail.com>
2024-12-03 13:21:14 +0100

    fix bug for new users

--- prototype.py
+++ prototype.py
@@ -144,120 +144,119 @@ else:
             st.subheader('Chat History')
             st_util.display_chat(session_data['messages'])
             st_util.display_feedback(session_data)
-    else:
-        # Title
-        st.title("AI Tutor")
-        if not st.session_state.conversation_active:
-            if st.session_state.session_id:
-                st_util.display_chat(st.session_state.chat_history)
+    # Title
+    st.title("AI Tutor")
+    if not st.session_state.conversation_active:
+        if st.session_state.session_id:
+            st_util.display_chat(st.session_state.chat_history)
+            st.session_state.model = Model(
+                system_prompt=(
+                """
+                    This is a feedback summary
+                    You should add feedback based on the conversation {chat_history}
+                    You should add the following;
+                    1. Conversation Summary
+                    2. Compliments for users based on the conversation
+                    3. Grammatical Feedback
+                    4. Suggestions for Future Practice
+                """
+                )
+            )
+            feedback = st.session_state.model.feedback(st.session_state.chat_history)
+            st.write("## Feedback Summary")
+            st.write(feedback)
+            db_util.save_feedback(user_id, st.session_state.session_id, feedback)
+        else:
+            text_prompt = st.text_input(label='Roleplay scenario description (optional)')
+            if st.button('Begin Conversation'):
+                st.session_state.conversation_active = True
+                st.session_state.session_id = str(uuid.uuid4())
+                if text_prompt:
+                    db_util.save_chat_settings(user_id, st.session_state.session_id, {'text_prompt': text_prompt})
+                else:
+                    text_prompt = "Imagine you met someone at a social event in France, and you don't know anything about them. Start by introducing yourself in French, and then respond to their questions and initiate topics of conversation."
                 st.session_state.model = Model(
                     system_prompt=(
-                    """
-                        This is a feedback summary
-                        You should add feedback based on the conversation {chat_history}
-                        You should add the following;
-                        1. Conversation Summary
-                        2. Compliments for users based on the conversation
-                        3. Grammatical Feedback
-                        4. Suggestions for Future Practice
-                    """
+                        f"""
+                        You are a French language tutor. As a native French speaker, you will be speaking with a tutee who wants to improve their French skills through a simulated conversation. 
+                        Additionally, develop a response generation strategy for introducing subtle corrections to your answers when the provided information is unclear or incorrect.
+                        Memorize any mistakes made during the conversation and provide a comprehensive report of errors at the conclusion of the discussion, detailing the corrections and explanations for the corrections. Go!
+                        
+                        Additionally, you are role-playing a real-life scenario that your tutee has specified. 
+                        The tutee described the following scenario: "{text_prompt}".
+                        You must play a suited role in the given scenario and interact with the tutee.
+
+                        NOTES:
+                        - Do not wait for the user to start speaking. Start by introducing yourself in French, and then respond to their questions and initiate topics of conversation. 
+                        - You should output your response in this format: <response> | <list of errors and their corrections in English>.
+                        - If there are possible suggestion on rephrasing a tutee's sentence to sound more native, explain the alternative phrase in the error section. If there are any suggestions, explain why it is better.
+                        - If there are no errors or suggestions, state "Good job! You made no errors." in English after the "|" symbol.
+                        - Write as you would speak. Be conversational and informal.
+                        - Provide concise responses, and adapt your tone and language to the level of the person you're speaking with.
+                        - You should not ask more than 2 questions on the same topic.
+                        - You should be engaging in the conversation by saying your opinion (do not do this every time you answer. Spice it up!).
+                        - You should be engaging in the conversation by telling anecdotes that happened to you (do not do this every time you answer. Spice it up!).
+                        - Ignore character errors such as using 'c' instead of 'ç' or oe instead of œ.
+                        """
                     )
                 )
-                feedback = st.session_state.model.feedback(st.session_state.chat_history)
-                st.write("## Feedback Summary")
-                st.write(feedback)
-                db_util.save_feedback(user_id, st.session_state.session_id, feedback)
-            else:
-                text_prompt = st.text_input(label='Roleplay scenario description (optional)')
-                if st.button('Begin Conversation'):
-                    st.session_state.conversation_active = True
-                    st.session_state.session_id = str(uuid.uuid4())
-                    if text_prompt:
-                        db_util.save_chat_settings(user_id, st.session_state.session_id, {'text_prompt': text_prompt})
-                    else:
-                        text_prompt = "Imagine you met someone at a social event in France, and you don't know anything about them. Start by introducing yourself in French, and then respond to their questions and initiate topics of conversation."
-                    st.session_state.model = Model(
-                        system_prompt=(
-                            f"""
-                            You are a French language tutor. As a native French speaker, you will be speaking with a tutee who wants to improve their French skills through a simulated conversation. 
-                            Additionally, develop a response generation strategy for introducing subtle corrections to your answers when the provided information is unclear or incorrect.
-                            Memorize any mistakes made during the conversation and provide a comprehensive report of errors at the conclusion of the discussion, detailing the corrections and explanations for the corrections. Go!
-                            
-                            Additionally, you are role-playing a real-life scenario that your tutee has specified. 
-                            The tutee described the following scenario: "{text_prompt}".
-                            You must play a suited role in the given scenario and interact with the tutee.
-
-                            NOTES:
-                            - Do not wait for the user to start speaking. Start by introducing yourself in French, and then respond to their questions and initiate topics of conversation. 
-                            - You should output your response in this format: <response> | <list of errors and their corrections in English>.
-                            - If there are possible suggestion on rephrasing a tutee's sentence to sound more native, explain the alternative phrase in the error section. If there are any suggestions, explain why it is better.
-                            - If there are no errors or suggestions, state "Good job! You made no errors." in English after the "|" symbol.
-                            - Write as you would speak. Be conversational and informal.
-                            - Provide concise responses, and adapt your tone and language to the level of the person you're speaking with.
-                            - You should not ask more than 2 questions on the same topic.
-                            - You should be engaging in the conversation by saying your opinion (do not do this every time you answer. Spice it up!).
-                            - You should be engaging in the conversation by telling anecdotes that happened to you (do not do this every time you answer. Spice it up!).
-                            - Ignore character errors such as using 'c' instead of 'ç' or oe instead of œ.
-                            """
-                        )
-                    )
-                    first_message = st.session_state.model.first_interaction()
-                    audio = st_util.stream_tts(first_message)
-                    message = {"user": "assistant", "text": first_message, "audio_bytes": audio} 
-                    db_util.save_message(user_id, st.session_state.session_id, message)
-                    st.session_state.chat_history = [message]
-                    st.rerun()
-        else: 
-            st_util.display_chat(st.session_state.chat_history)            
-            if st.session_state.text_sent and st.session_state.audio_processed:
-                # Reset flags for new audio input
-                st.session_state.text_sent = False
-                st.session_state.audio_processed = False
-        
-            if not st.session_state.text_sent and not st.session_state.audio_processed and st.button("Finish Conversation"):
-                st_util.restart_conversation(session_id=st.session_state.session_id, chat_history=st.session_state.chat_history)            
-        
-            if not st.session_state.audio_processed:
-                audio_bytes = audio_recorder(icon_size="4x", pause_threshold=7)
-                if audio_bytes:
-                    # Ensure the audio processing happens only once
-                    if not st.session_state.audio_processed:
-                        # Process the audio to text
-                        st.markdown("Processing audio...")
-                        if not TEST_MODE:
-                            text = process_speech_bytes_to_text('wav', audio_bytes, 'audio/wav', lang='fr')
-                            message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
-                        else:
-                            text = 'Sample text input.'
-                            message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
-                        db_util.save_message(user_id, st.session_state.session_id, message)
-                        st.session_state.chat_history.append(message)
-                        st.session_state.audio_processed = True  # Set flag to True after processing
-                        audio_bytes = None
-                        st.session_state.audio_text = text  # Store the processed text in session state
-                        st.rerun()
-
-            # Step 2: Send the text to chatbot model
-            if "audio_text" in st.session_state and st.session_state.audio_processed and not st.session_state.text_sent:
-                if not TEST_MODE:
-                    response, errors = st.session_state.model.process(st.session_state.audio_text)
-                else:
-                    response = 'Bonjour, comment allez-vous? Voulez-vous apprendre le français?'
-                    errors = ''
-                st.session_state.text_sent = True  # Set flag to True after response is generated
-                # Step 3: Convert the chatbot's response to speech and play
-                st.markdown("Playing response...")
-                audio = st_util.stream_tts(response)
-                message = {"user": "assistant", "text": response, "audio_bytes": audio}
-                error_message = {"user": "assistant", "text": "Errors identified: " + errors, "audio_bytes": None}
-                #st_util.display_message(message)
-                st.session_state.chat_history.append(message)
-                st.session_state.chat_history.append(error_message)
+                first_message = st.session_state.model.first_interaction()
+                audio = st_util.stream_tts(first_message)
+                message = {"user": "assistant", "text": first_message, "audio_bytes": audio} 
                 db_util.save_message(user_id, st.session_state.session_id, message)
-                db_util.save_message(user_id, st.session_state.session_id, error_message)
+                st.session_state.chat_history = [message]
                 st.rerun()
+    else: 
+        st_util.display_chat(st.session_state.chat_history)            
+        if st.session_state.text_sent and st.session_state.audio_processed:
+            # Reset flags for new audio input
+            st.session_state.text_sent = False
+            st.session_state.audio_processed = False
+    
+        if not st.session_state.text_sent and not st.session_state.audio_processed and st.button("Finish Conversation"):
+            st_util.restart_conversation(session_id=st.session_state.session_id, chat_history=st.session_state.chat_history)            
+    
+        if not st.session_state.audio_processed:
+            audio_bytes = audio_recorder(icon_size="4x", pause_threshold=7)
+            if audio_bytes:
+                # Ensure the audio processing happens only once
+                if not st.session_state.audio_processed:
+                    # Process the audio to text
+                    st.markdown("Processing audio...")
+                    if not TEST_MODE:
+                        text = process_speech_bytes_to_text('wav', audio_bytes, 'audio/wav', lang='fr')
+                        message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
+                    else:
+                        text = 'Sample text input.'
+                        message = {"user": "user", "text": text, "audio_bytes": audio_bytes}
+                    db_util.save_message(user_id, st.session_state.session_id, message)
+                    st.session_state.chat_history.append(message)
+                    st.session_state.audio_processed = True  # Set flag to True after processing
+                    audio_bytes = None
+                    st.session_state.audio_text = text  # Store the processed text in session state
+                    st.rerun()
 
-            if st.session_state.conversation_active and st.session_state.text_sent:
-                # Reset flags for new audio input
-                st.session_state.text_sent = False
-                st.session_state.audio_processed = False
+        # Step 2: Send the text to chatbot model
+        if "audio_text" in st.session_state and st.session_state.audio_processed and not st.session_state.text_sent:
+            if not TEST_MODE:
+                response, errors = st.session_state.model.process(st.session_state.audio_text)
+            else:
+                response = 'Bonjour, comment allez-vous? Voulez-vous apprendre le français?'
+                errors = ''
+            st.session_state.text_sent = True  # Set flag to True after response is generated
+            # Step 3: Convert the chatbot's response to speech and play
+            st.markdown("Playing response...")
+            audio = st_util.stream_tts(response)
+            message = {"user": "assistant", "text": response, "audio_bytes": audio}
+            error_message = {"user": "assistant", "text": "Errors identified: " + errors, "audio_bytes": None}
+            #st_util.display_message(message)
+            st.session_state.chat_history.append(message)
+            st.session_state.chat_history.append(error_message)
+            db_util.save_message(user_id, st.session_state.session_id, message)
+            db_util.save_message(user_id, st.session_state.session_id, error_message)
+            st.rerun()
+
+        if st.session_state.conversation_active and st.session_state.text_sent:
+            # Reset flags for new audio input
+            st.session_state.text_sent = False
+            st.session_state.audio_processed = False
